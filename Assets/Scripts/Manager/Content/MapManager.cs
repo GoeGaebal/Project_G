e141993030c75c40e52ffe8b304b53ea09fcf924
Yun yeobeom <bangga0403@gmail.com>
2023-03-22 23:49:37 +0900

    refactor: Move codes to LobbyScene from LobbyManager
    스크립트를 보다 구조화하기 위해 Manager, Scenes, Util 폴더로 나누고 LobbyManager의 코드를 LobbyScene으로 이동

--- Assets/Scripts/Manager/Content/MapManager.cs
+++ Assets/Scripts/Manager/Content/MapManager.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour
+public class MapManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public Grid CurrentGrid { get; private set; }
+    public void LoadMap(int mapId)
     {
+        DestoryMap();
+        // 1인 경우 000 중 마지막 0만 1로 바꿈
+        string mapName = "Map_" + mapId.ToString("000");
+        GameObject go = Managers.Resource.Instantiate($"Map/{mapName}");
+        go.name = "Map";
         
+        CurrentGrid = go.GetComponent<Grid>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void DestoryMap()
     {
-        
+        GameObject map = GameObject.Find("Map");
+        if (map != null)
+        {
+            GameObject.Destroy(map);
+            CurrentGrid = null;
+        }
     }
 }
---
+++ Assets/Scripts/Manager/Core/SceneManagerEx.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Scene 관련 매니저
+/// </summary>
+public class SceneManagerEx
+{
+    public BaseScene CurrentScene { get { return GameObject.FindObjectOfType<BaseScene>(); } }
+
+    /// <summary>
+    /// Scene을 불러오는 함수, 기존 Unity 방식으로는 type에 따라서 불러오지 못하므로 이를 반영하였다.
+    /// </summary>
+    /// <remark>
+    /// PUN2를 위해 SceneManager.LoadScene이 아닌 PhotonNetwork.LoadLevel을 사용하였다.
+    /// </remark>
+    /// <param name="type">
+    /// Define에 존재하는 Scene enum class에 존재하는 상수
+    /// /param>
+	public void LoadScene(Define.Scene type)
+    {
+        Managers.Clear();
+
+        // SceneManager.LoadScene(GetSceneName(type));
+        PhotonNetwork.LoadLevel(GetSceneName(type));
+    }
+
+    string GetSceneName(Define.Scene type)
+    {
+        string name = System.Enum.GetName(typeof(Define.Scene), type);
+        return name;
+    }
+
+    public void Clear()
+    {
+        CurrentScene.Clear();
+    }
+}
