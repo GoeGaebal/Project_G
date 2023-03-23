44169b5d0c3402df4ec8861dbcd71394ce166a2d
Yun yeobeom <bangga0403@gmail.com>
2023-03-23 20:12:49 +0900

    refactor: Move code to scene from managers
    LobbyManager와 GameManager에 존재하던 코드들을 각각의 Scene에 옮김

--- Assets/Scripts/Manager/Content/MapManager.cs
+++ Assets/Scripts/Manager/Content/MapManager.cs
@@ -4,24 +4,40 @@ using UnityEngine;
 
 public class MapManager
 {
+    /// <summary>
+    /// 현재 Grid 즉 맵
+    /// </summary>
     public Grid CurrentGrid { get; private set; }
+    
+    /// <summary>
+    /// 프리팹에서 맵을 생성하는 함수
+    /// </summary>
+    /// <remarks>
+    /// 맵은 Prefab의 Map폴더에 위치시켜야 하며 Map_000 형태로 저장된다.
+    /// </remarks>>
+    /// <param name="mapId">
+    /// int형 변수, Map_001을 인스턴트화 하고 싶으면 1을 넘기면 됨
+    /// </param>
     public void LoadMap(int mapId)
     {
         DestoryMap();
         // 1인 경우 000 중 마지막 0만 1로 바꿈
         string mapName = "Map_" + mapId.ToString("000");
-        GameObject go = Managers.Resource.Instantiate($"Map/{mapName}");
+        GameObject go = Managers.Resource.Instantiate($"Map/{mapName}", new Vector3(0,0,0),Quaternion.identity);
         go.name = "Map";
         
         CurrentGrid = go.GetComponent<Grid>();
     }
     
+    /// <summary>
+    /// Hierarchy창에서 Map 오브젝트를 찾아 Destory하는 함수
+    /// </summary>
     public void DestoryMap()
     {
         GameObject map = GameObject.Find("Map");
         if (map != null)
         {
-            GameObject.Destroy(map);
+            Object.Destroy(map);
             CurrentGrid = null;
         }
     }
--- Assets/Scripts/Manager/Core/ResourceManager.cs
+++ Assets/Scripts/Manager/Core/ResourceManager.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class ResourceManager : MonoBehaviour
+public class ResourceManager
 {
+    /// <summary>
+    /// Resources 폴더 내 자료를 Hierarchy창에 올린다.
+    /// 만일 Object라면 이미 Pool에 올라와 있는 경우 Pool에서
+    /// 꺼내 쓰도록 한다.
+    /// </summary>
+    /// <param name="path">
+    /// Resources 폴더 내의 경로를 의미한다.
+    /// </param>
+    /// <typeparam name="T">
+    /// UnityEngine의 Object 클래스에 한해서만 받는다
+    /// </typeparam>
+    /// <returns></returns>
     public T Load<T>(string path) where T : Object
     {
         if (typeof(T) == typeof(GameObject))
@@ -21,7 +34,16 @@ public class ResourceManager : MonoBehaviour
         return Resources.Load<T>(path);
     }
 
-    public GameObject Instantiate(string path, Transform parent = null)
+    /// <summary>
+    /// Prefabs 폴더에 존재하는 프리팹을 인스턴스화 한다.
+    /// </summary>
+    /// <remarks> PUN2의 PhotonNetwork.Instantiate가 아닌 Resources.Instantiatea를 사용하였다.</remarks>
+    /// <param name="path">Resources/Prefab 폴더 내의 경로를 의미한다.</param>
+    /// <param name="position">생성할 프리팹의 위치를 의미한다</param>
+    /// <param name="rotation">생성할 프리팹의 rotation을 의미한다</param>
+    /// <param name="parent">만일 어떤 객체의 자식을 할당할 것이라면 그 부모가 될 객체이다.</param>
+    /// <returns></returns>
+    public GameObject Instantiate(string path, Vector3 position, Quaternion rotation ,Transform parent = null)
     {
         GameObject original = Load<GameObject>($"Prefabs/{path}");
         if (original == null)
@@ -33,11 +55,19 @@ public class ResourceManager : MonoBehaviour
         if (original.GetComponent<Poolable>() != null)
             return Managers.Pool.Pop(original, parent).gameObject;
 
-        GameObject go = Object.Instantiate(original, parent);
+        // TODO : PhotonNetwork는 자체적으로 Pool을 사용하므로 추후에 문제가 없는지 확인할 것
+        GameObject go = Object.Instantiate(original, position, rotation, parent);
+        // GameObject go = PhotonNetwork.Instantiate($"Prefabs/{path}", position, rotation);
         go.name = original.name;
         return go;
     }
     
+    /// <summary>
+    /// 객체가 Pool
+    /// </summary>
+    /// <param name="go">
+    /// 파괴할 객체를 의미한다.
+    /// </param>
     public void Destroy(GameObject go)
     {
         if (go == null)
