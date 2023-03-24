be8d5cc69addfdce79797e2a6e6430b69aca746b
Haerang Lee <82368502+LHR-kr@users.noreply.github.com>
2023-03-25 01:48:52 +0900

    feat: 고블린 idle 애니메이션과 run 애니메이션 연결
    flip이 제대로 안 되는 문제 해결해야 함

--- Assets/Scripts/Enemy/BasicEnemy.cs
+++ Assets/Scripts/Enemy/BasicEnemy.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class BasicEnemy : DamageableEntity
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] protected float attackPoint; 
+    [SerializeField] protected float attackCooldown;
+    [SerializeField] private float detectRadius;
+    [SerializeField] private LayerMask targetLayerMask;
+    [SerializeField] protected Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
-    // Update is called once per frame
-    void Update()
-    {
+    protected bool hasTarget = false;
+    protected Vector2 targetPos;
+
+    private void Update() {
+        Collider2D playerCollider = Physics2D.OverlapCircle(gameObject.transform.position, detectRadius,targetLayerMask);
+        if(playerCollider != null && animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            animator.SetTrigger("triggerRun");
+            Vector2 playerpos = playerCollider.transform.position;
+            if(!hasTarget)
+            {
+                hasTarget = true;
+                targetPos = playerCollider.transform.position;
+            }
+        
+        }
+        else if(playerCollider == null)
+        {
+            hasTarget = false;
+            targetPos = transform.position;
+            animator.SetTrigger("triggerIdle");
+        }
+        if(hasTarget)
+        {
+            if(targetPos.x < transform.position.x) spriteRenderer.flipX = true;
+            else spriteRenderer.flipX = false;
+        }
         
     }
 }
