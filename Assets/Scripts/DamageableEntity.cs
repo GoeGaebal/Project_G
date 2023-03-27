0e320e92a10e6ca95fbfc04b0b142bc279711b81
Haerang Lee <82368502+LHR-kr@users.noreply.github.com>
2023-03-27 16:39:32 +0900

    feat: 몬스터 피격 애니메이션 추가
    고블린. 머쉬룸, 플라잉아이, 스켈레톤

--- Assets/Scripts/DamageableEntity.cs
+++ Assets/Scripts/DamageableEntity.cs
@@ -5,14 +5,14 @@ using System;
 
 public class DamageableEntity : MonoBehaviour, IDamageable
 {
-    public float maxHP {get; protected set;}
+    public float maxHP;
     public float HP{get;protected set;} // 기존의 getter, setter 메소드를 대체한다, get은 public , set은 protected으로 접근제어를 한다.
 
     public bool isDead {get;protected set;}
 
     // event는 Action이 이 클래스 외부에서 실행되는 것을 방지한다.
     // 그리고 Ondeath가 호출되어야 하는 시점에 어떤 행동을 해야 하는지 런타임 단계에서 정할 수 있다.
-    public event Action onDeath; 
+    public event Action dieAction; 
 
     protected virtual void OnEnable() { // 객체가 enable 될 때 호출되는 함수
         // 체력이랑 isDead 변수 초기화, 살아 있는 상태로 만든다.
@@ -51,10 +51,11 @@ public class DamageableEntity : MonoBehaviour, IDamageable
     { 
         if (isDead) return;
         
-        if(onDeath != null)
+        if(dieAction != null)
         {
-            onDeath();  
+            dieAction();  
         }
+        Debug.Log("Die");
         isDead = true;
     }
 }
