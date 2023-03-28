d08d0398eeaeeaf6dbfc602d8eb22ed32d4bc2fb
KSangMin <fhfpszmfnwm@naver.com>
2023-03-28 17:32:02 +0900

    feat: 아이템 클래스 구현
    Scriptable Object 사용

--- Assets/Scripts/Inventory/ItemInSlot.cs
+++ Assets/Scripts/Inventory/ItemInSlot.cs
@@ -7,8 +7,20 @@ using UnityEngine.EventSystems;
 public class ItemInSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [SerializeField] private Image _icon;
-    [HideInInspector] public Transform parentAfterDrag;
+    
+    [HideInInspector] public Item item;
+    [HideInInspector] private Transform parentAfterDrag;
 
+    private void Start()
+    {
+        InitializeItem(item);
+    }
+
+    public void InitializeItem(Item newItem)
+    {
+        item = newItem;
+        _icon.sprite = newItem.Icon;
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
         _icon.raycastTarget = false;
