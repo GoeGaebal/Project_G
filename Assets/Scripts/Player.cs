6802541ba72c97845eb90f86bfd4852a172f75b7
Haerang Lee <82368502+LHR-kr@users.noreply.github.com>
2023-04-02 03:47:19 +0900

    feat: 플레이어 이동 애니메이션 전환

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,19 +7,31 @@ using UnityEngine.InputSystem;
 
 public enum EnumPlayerStates
 {
-    Idle, Attack1, Attack2, Attack3, Run
+    Idle, Attack1, Attack2, Attack3, Run, Hit
 }
 
 public class Player : DamageableEntity
 {
     [SerializeField]private float moveSpeed = 5.0f;
-
+    private InputAction inputAction; 
+    private PlayerInput playerInput; 
     private Vector2 moveInput;
     private Rigidbody2D rb;
     protected SpriteRenderer spriteRenderer;
     protected Animator animator;
 
-    private 
+    private EnumPlayerStates _state;
+
+    public EnumPlayerStates State
+    {
+        get{
+            return _state;
+        }
+        protected set{
+                _state = value;
+                UpdateState();
+        }
+    }
 
 
     // Start is called before the first frame update
@@ -28,11 +40,20 @@ public class Player : DamageableEntity
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-    }
+        playerInput = GetComponent<PlayerInput>();
 
+        inputAction = playerInput.actions["Move"];
+
+        Debug.Log(inputAction == null);
+
+    }
+    
     private void FixedUpdate()
     {
-        
+        if(State == EnumPlayerStates.Idle && inputAction.IsPressed())
+            State = EnumPlayerStates.Run;
+        else if(State == EnumPlayerStates.Run && !inputAction.IsPressed())
+            State = EnumPlayerStates.Idle;
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
 
@@ -41,6 +62,13 @@ public class Player : DamageableEntity
         if (photonView.IsMine)
         {
             moveInput = value.Get<Vector2>();
+            if(moveInput == null) return;
+
+            if(moveInput.x >0) spriteRenderer.flipX = false;
+            else if (moveInput.x <0) spriteRenderer.flipX = true;
+
+            
+            
         }
     }
 
@@ -49,6 +77,21 @@ public class Player : DamageableEntity
         if (photonView.IsMine)
         {
             Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y,-10);
+        }
+    }
+
+    private void UpdateState()
+    {
+        switch (State)
+        {
+            case EnumPlayerStates.Idle:
+                animator.SetBool("run",false);
+                break;
+            case EnumPlayerStates.Run:
+                animator.SetBool("run",true);
+                break;
+            default:
+                break;
         }
     }
 }
