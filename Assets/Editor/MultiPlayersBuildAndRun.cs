6b1621522018e3053b31d469631fcf7f5aa12bc9
Yeobeom <75617688+Yeobeom@users.noreply.github.com>
2023-04-07 17:26:44 +0900

    feat: MultiPlayerBuiladAndRun 생성
    Tools 에 Multiplayer 인원 수를 결정하면 개수 만큼 빌드 후 실행

---
+++ Assets/Editor/MultiPlayersBuildAndRun.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+#endif
+
+public class MultiPlayersBuildAndRun
+{
+    [MenuItem("Tools/Run Multiplayer/2 Players")]
+    static void PerformWin64Build2()
+    {
+        PerformWin64Build(2);
+    }
+    
+    [MenuItem("Tools/Run Multiplayer/3 Players")]
+    static void PerformWin64Build3()
+    {
+        PerformWin64Build(3);
+    }
+
+    
+    [MenuItem("Tools/Run Multiplayer/4 Players")]
+    static void PerformWin64Build4()
+    {
+        PerformWin64Build(4);
+    }
+
+
+    static void PerformWin64Build(int playerCount)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(
+            BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows);
+
+        for (int i = 1; i <= playerCount; i++)
+        {
+            BuildPipeline.BuildPlayer(GetScenePahts(),
+                "Builds/Win64/" + GetProjectName() + i.ToString() + "/" + GetProjectName() + i.ToString() + ".exe",
+                BuildTarget.StandaloneWindows64, BuildOptions.AutoRunPlayer);
+        }
+    }
+
+    static string GetProjectName()
+    {
+        string[] s = Application.dataPath.Split("/");
+        return s[s.Length - 2];
+    }
+
+    static string[] GetScenePahts()
+    {
+        string[] scenes = new string[EditorBuildSettings.scenes.Length];
+
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            scenes[i] = EditorBuildSettings.scenes[i].path;
+        }
+        return scenes;
+    }
+}
