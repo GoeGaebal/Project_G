23bc23d7a6eb095f30aeb6c8d4077e0ca48ef4d3
Yeobeom <75617688+Yeobeom@users.noreply.github.com>
2023-04-07 13:42:53 +0900

    fix: 뿌릴 때 공중에 뜨지 않던 버그 수정
    이유는 모르겠지만 Start에 미리 Sn(충돌 계수 등비수열의 합)을 연산하였더니 적용이 안 됨. Coroutine 시작 시에 계산하도록 함. 추후에 데이터베이스에서 바로 꺼낼 수 있도록 함

--- Assets/Scripts/Managers/Content/ObjectManager.cs
+++ Assets/Scripts/Managers/Content/ObjectManager.cs
@@ -25,16 +25,25 @@ public class ObjectManager
         }
     }
 
-    public void SpawnLootings(int count, Vector3 spawnPos, float radious = 1.0f)
+    /// <summary>
+    /// 아이템을 뿌려주는 메소드
+    /// </summary>
+    /// <param name="count">아이템을 촘 몇개 뿌릴 것인지</param>
+    /// <param name="spawnPos">뿌려지는 시작 장소</param>
+    /// <param name="maxRadious">최대 거리</param>
+    /// <param name="minRadious">최소 거리</param>
+    public void SpawnLootings(int count, Vector3 spawnPos, float maxRadious = 10.0f,float minRadious = 0.0f)
     {
         for (int i = 0; i < count; i++)
         {
-            Vector2 randPos = Random.insideUnitCircle * radious;
+            Vector2 randPos = Random.insideUnitCircle * Random.Range(minRadious,maxRadious);
             randPos.x += spawnPos.x;
             randPos.y += spawnPos.y;
             GameObject go =  Managers.Resource.Instantiate("Gathering/Coin", spawnPos, Quaternion.identity);
-            GatheringController gc = go.GetComponent<GatheringController>();
-            gc.Bouncing(randPos, 1.5f);
+            // TODO : GetComponent라는 비싼 연산을 수행하며 시간과 거리가 하드코딩 되어있음.
+            // 아마 오브젝트 풀링을 적용하면 조금은 해결될 것
+            LootingItemController lc = go.GetComponent<LootingItemController>();
+            lc.Bounce(randPos, 1.0f, 1.0f);
         }
     }
 }
