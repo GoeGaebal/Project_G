06bd30120fc180b7ce29dd2d79e28fed778ac26a
Yeobeom <75617688+Yeobeom@users.noreply.github.com>
2023-04-07 11:21:40 +0900

    feat: Implement BouncingPosition
    물리적으로 통통 튀는 연출 구현

--- Assets/Scripts/Controller/Gathering/GatheringController.cs
+++ Assets/Scripts/Controller/Gathering/GatheringController.cs
@@ -1,8 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GatheringController : DamageableEntity
 {
+    private Vector3 endPosition = new Vector3(2,0,0);
+
+    private float currentTIme = 0;
+    private float lerpTime = 1.0f;
+    private void Start()
+    {
+        transform.position = Vector3.zero;
+        StartCoroutine(LerpPosition(endPosition, lerpTime));
+    }
+
+    private void Update()
+    {
+        
+    }
     
+    IEnumerator BouncingPosition(Vector3 targetPosition,float duration, float cof = 0.6f, int bounceCount = 5, float threshold = 1.0f)
+    {
+        // 초기 값 세팅
+        float time = 0.0f;
+        float curH = 0.0f;
+        float Sn = (1.0f - Mathf.Pow(cof, bounceCount)) / (1.0f - cof);
+        
+        float hSpeed = 10.0f;
+        float gravity = (2.0f * hSpeed * Sn) / duration;
+        Vector3 startPosition = transform.position;
+        
+        while (time < duration)
+        {
+            // apply gravity if the entity is in the air:
+            if (curH > 0) hSpeed -= gravity * Time.deltaTime;
+            // add speed to altitude:
+            curH += hSpeed * Time.deltaTime;
+            // if the entity did hit the ground:
+            if (curH < 0) {
+                // simulate a bounce (to avoid negative Z):
+                curH = -curH;
+                // if the entity was falling down, reduce the speed: 
+                if (hSpeed < 0.0f) hSpeed = -hSpeed * cof;
+                // if the speed is now below the threshold, snap the entity to the ground:
+                if (hSpeed < threshold) {
+                    curH = 0.0f;
+                    hSpeed = 0.0f;
+                }
+            }
+            
+            float t = time / duration;
+            t = Mathf.Sin(t * Mathf.PI * 0.5f);
+            transform.position = Vector3.up * curH + Vector3.Lerp(startPosition, targetPosition, t);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+    }
 }
