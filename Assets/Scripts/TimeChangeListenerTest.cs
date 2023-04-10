addbff87ad6c0182efdfdf6ff14b76bc6eee4f88
Haerang Lee <82368502+LHR-kr@users.noreply.github.com>
2023-04-10 22:34:46 +0900

    feat: 시간대 바뀌면 카메라 색감 바뀜

--- Assets/Scripts/TimeChangeListenerTest.cs
+++ Assets/Scripts/TimeChangeListenerTest.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class TimeChangeListenerTest : MonoBehaviour, ITimeSlotChangeEventListener
 {
     public TimeSlotManager timeSlotManager;
+    public PostProcessVolume postProcessVolume;
+
+    private ColorGrading colorGrading;
+    public PostProcessProfile profile;
     // Start is called before the first frame update
     void Start()
     {
         this.AssignEventHandler();
-    }
+        this.postProcessVolume = GetComponent<PostProcessVolume>();
+
+        profile.TryGetSettings(out colorGrading);
+    }   
 
 
 
@@ -18,9 +26,11 @@ public class TimeChangeListenerTest : MonoBehaviour, ITimeSlotChangeEventListene
         switch(timeSlot)
         {
             case EnumTimeSlot.Day:
+                this.colorGrading.colorFilter.value = new Color(1.0f, 1.0f, 1.0f,1.0f);
                 Debug.Log("day time test");
                 break;
             case EnumTimeSlot.Night:
+                this.colorGrading.colorFilter.value = new Color(0.243592f, 0.2473907f, 0.3018868f,1.0f);
                 Debug.Log("night time test");
                 break;
         }
