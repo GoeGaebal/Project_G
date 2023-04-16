844a12b97149d081cd8e3bd3121de5cfce50075b
KSangMin <fhfpszmfnwm@naver.com>
2023-04-16 14:39:33 +0900

    feat: 마우스 휠 스크롤로 퀵슬롯 상에서 현재 선택된 아이템 변경 기능 구현
    feat: 마우스 휠 스크롤로 퀵슬롯 상에서 현재 선택된 아이템 변경 기능 구현

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -6,42 +6,52 @@ public class InventoryManager : MonoBehaviour
 {
     public int maxStackedItems = 4;
     public Slot[] slots;
+    public Slot[] quickslots;
     public GameObject IconPrefab;//InventoryItemPrefab
 
-    int selectedSlot = -1;//현재 선택된 퀵슬롯
+    public int selectedSlot = -1;
+    //현재 선택된 퀵슬롯
 
     private void Start()
     {
-        ChangeSelectedSlot(0);
+        ChangeSelectedQuickSlot(0);
     }
 
     private void Update()
     {//키입력에 따른 퀵슬롯 선택 변화(추후 New Input System으로 변경)
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            ChangeSelectedSlot(0);
+            ChangeSelectedQuickSlot(0);
         } else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            ChangeSelectedSlot(1);
+            ChangeSelectedQuickSlot(1);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            ChangeSelectedSlot(2);
+            ChangeSelectedQuickSlot(2);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            ChangeSelectedSlot(3);
+            ChangeSelectedQuickSlot(3);
         }
     }
 
-    void ChangeSelectedSlot(int newValue)//선택된 슬롯 색깔 변경
+    public void ChangeSelectedQuickSlot(int newValue)//선택된 슬롯 색깔 변경
     {
+        if (newValue < 0)
+        {
+            newValue += quickslots.Length;
+        }
+        else if (newValue >= quickslots.Length)
+        {
+            newValue -= quickslots.Length;
+        }
         if (selectedSlot >= 0)
         {
-            slots[selectedSlot].Deselect();
+            quickslots[selectedSlot].Deselect();
         }
-        
-        slots[newValue].Select();
+
+        quickslots[newValue].Select();
         selectedSlot = newValue;
     }
 
@@ -71,7 +81,7 @@ public class InventoryManager : MonoBehaviour
         return false;//인벤토리 빈 공간 없음
     }
 
-    void SpawnNewItem(Item item, Slot slot)
+    public void SpawnNewItem(Item item, Slot slot)
     {
         GameObject newItemGo = Instantiate(IconPrefab, slot.transform);//테스트용
         ItemInSlot itemInSlot = newItemGo.GetComponent<ItemInSlot>();
