326675143f67b72040728ae1ec1e279463f8c3fa
Haerang Lee <82368502+LHR-kr@users.noreply.github.com>
2023-04-26 03:25:57 +0900

    feat: 공격 애니메이션 제작

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -26,9 +26,8 @@ public class WeaponController : MonoBehaviour
     private IWeapon meleeWeaponController;
     private IWeapon pickaxWeaponController;
 
-    private GameObject playerGameObject;
     private SpriteRenderer spriteRenderer;
-    [SerializeField]private float disFromBody; 
+    
     private void Awake() {
        
     }
@@ -38,33 +37,12 @@ public class WeaponController : MonoBehaviour
 
         spriteRenderer = GetComponent<SpriteRenderer>();
         
-        playerGameObject = transform.root.gameObject;
+
 
         weaponController = meleeWeaponController;
     }
 
-    private void Update() {
-          //Get the Screen positions of the object
-         Vector3 positionOnScreen = playerGameObject.transform.position;
-         
-         //Get the Screen position of the mouse
-         Vector3 mouseOnScreen = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-         
-         //Get the angle between the points
-         float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen) + 90.0f; //90도 보정
- 
-         //Rotate the object to face the mouse
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-        Debug.Log(angle);
 
-        transform.position = new Vector3(-disFromBody * Mathf.Sin(angle * Mathf.Deg2Rad), disFromBody* Mathf.Cos(angle* Mathf.Deg2Rad),0) + transform.root.position;
-
-         float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
-         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-        }
-
-        
-    }
    
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -90,7 +68,7 @@ public class WeaponController : MonoBehaviour
 
     private void AddLayerMask(int layerNum)
     {
-
+        
     }
     private void RemoveLayerMask(int layerNum)
     {
@@ -102,19 +80,20 @@ public class WeaponController : MonoBehaviour
        
         if(context.ReadValue<float>() > 0 && context.started)
         {
-         Debug.Log("wheel up");
-                spriteRenderer.sprite = axeSprite;
-                spriteRenderer.sortingOrder = 3;
+            Debug.Log("wheel up");
+            spriteRenderer.sprite = axeSprite;
+                
             
             
         }
         else if(context.ReadValue<float>() < 0 && context.started)
         {
-              Debug.Log("wheel down");
-                spriteRenderer.sprite = swordSprite;
-                spriteRenderer.sortingOrder = 3;
+            Debug.Log("wheel down");
+            spriteRenderer.sprite = swordSprite;
+
                 
         }
+        spriteRenderer.sortingOrder = 3;
     }
 
 }
