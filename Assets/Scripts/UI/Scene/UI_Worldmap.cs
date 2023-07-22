b487422ab6632fc1fe2ebb8262489d668a69ce11
KSangMin <fhfpszmfnwm@naver.com>
2023-07-22 15:30:40 +0900

    feat: 월드맵_미니맵 UI 구현
    목표, 이동시간, 거리, 방향 표시

--- Assets/Scripts/UI/Scene/UI_Worldmap.cs
+++ Assets/Scripts/UI/Scene/UI_Worldmap.cs
@@ -15,7 +15,13 @@ public class UI_Worldmap : UI_Scene
         Worldmap_Background,
         Worldmap_Ship,
         Worldmap_Line,
-        Weather
+        WeatherText,
+        Worldmap_Minimap_Background,
+        Worldmap_Minimap_Arrow,
+        Worldmap_Minimap_Ship,
+        Worldmap_Minimap_MovingText,
+        Worldmap_Minimap_TimeText,
+        Worldmap_Minimap_DistanceText
     }
     enum Buttons
     {
@@ -34,9 +40,14 @@ public class UI_Worldmap : UI_Scene
     private GameObject _lrGO;
     private GameObject _worldmap;
     private UILineRenderer _lr;
-    private GameObject _weather;
+    private GameObject _weatherText;
+    private GameObject _arrow;
+    private GameObject _minimapShip;
+    private GameObject _movingText;
+    private GameObject _timeText;
+    private GameObject _distanceText;
 
-    private bool _moveFlag = true;//이동 중
+    private bool _moveFlag = false;//이동 중
     private bool _arriveFlag = false;//도착
 
     private string _mapName;
@@ -53,18 +64,24 @@ public class UI_Worldmap : UI_Scene
             if (Vector2.Distance(_ship.transform.position, _target.transform.position) >= 1f)
             {
                 MoveToTarget();
+                _movingText.GetComponent<TMP_Text>().SetText(Managers.Data.WorldmapDict[_target.name[6] - '0'].name + "로 이동 중");
                 if (!CheckWeather())
                 {
-                    _weather.GetComponent<TMP_Text>().SetText("Nothing Detected!");
+                    _weatherText.GetComponent<TMP_Text>().SetText("Nothing Detected!");
                 }
             }
             else
             {
+                _moveFlag = false;
                 _arriveFlag = true;
+                _timeText.SetActive(false);
+                _distanceText.SetActive(false);
+                _arrow.SetActive(false);
             }
         }
         if (_arriveFlag && _mapName != "Worldmap_Ship")
         {
+            _movingText.GetComponent<TMP_Text>().SetText(Managers.Data.WorldmapDict[_target.name[6] - '0'].name + "에 정박 중");
             //TODO: 필드로 이동 가능하게 하는 코드
             //_mapName 이용해서 해당하는 맵 프리팹을 필드에 생성시키기.
         }
@@ -85,7 +102,15 @@ public class UI_Worldmap : UI_Scene
         _target = _ship;
         _lrGO = Get<GameObject>((int)GameObjects.Worldmap_Line);
         _lr = _lrGO.GetComponent<UILineRenderer>();
-        _weather = Get<GameObject>((int)GameObjects.Weather);
+        _arrow = Get<GameObject>((int)GameObjects.Worldmap_Minimap_Arrow);
+        _arrow.SetActive(false);
+        _minimapShip = Get<GameObject>((int)GameObjects.Worldmap_Minimap_Ship);
+        _weatherText = Get<GameObject>((int)GameObjects.WeatherText);
+        _movingText = Get<GameObject>((int)GameObjects.Worldmap_Minimap_MovingText);
+        _timeText = Get<GameObject>((int)GameObjects.Worldmap_Minimap_TimeText);
+        _timeText.SetActive(false);
+        _distanceText = Get<GameObject>((int)GameObjects.Worldmap_Minimap_DistanceText);
+        _distanceText.SetActive(false);
 
         GetButton((int)Buttons.Worldmap_Button).gameObject.BindEvent(OpenWorldmapUI);
         GetButton((int)Buttons.Worldmap_Button_Close).gameObject.BindEvent(CloseWorldmapUI);
@@ -103,10 +128,37 @@ public class UI_Worldmap : UI_Scene
 
     private void MoveToTarget()//목표 방향으로 이동
     {
-        float distance = Vector2.Distance(_ship.transform.position, _target.transform.position);
         Vector2 direction = _target.transform.position - _ship.transform.position;
-        _ship.transform.position += (Vector3)(direction / distance);
+        _ship.transform.position += (Vector3)(direction.normalized);
         SetLine();
+        if (direction.x >= 0)
+        {
+            //_ship.transform.localScale = new Vector3(1,1,1);
+        }
+        else
+        {
+            //_ship.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        SetTimeDistanceText(direction);
+        SetArrow(direction);
+    }
+
+    public void SetTimeDistanceText(Vector2 direction)//남은 시간, 남은 거리 갱신
+    {
+        _timeText.SetActive(true);
+        _distanceText.SetActive(true);
+        _timeText.GetComponent<TMP_Text>().SetText("남은 시간: " + (int)(direction.magnitude * Time.fixedDeltaTime) + "sec");
+        _distanceText.GetComponent<TMP_Text>().SetText("남은 거리: " + (int)(direction.magnitude) + "m");
+    }
+
+    public void SetArrow(Vector2 direction)//화살표 방향 갱신
+    {
+        _arrow.SetActive(true);
+        float angle = Vector2.SignedAngle(Vector2.up, direction);
+        Debug.Log(angle);
+        Quaternion rot = Quaternion.Euler(0f, 0f, angle);
+        _arrow.transform.rotation = rot;
     }
 
     private void SetLine()//배와 목표 사이 선 새로 긋기
@@ -130,6 +182,10 @@ public class UI_Worldmap : UI_Scene
         SetTarget(_ship);
         _moveFlag = false;
         _lr.enabled = false;
+        _movingText.GetComponent<TMP_Text>().SetText("정지 중");
+        _timeText.SetActive(false);
+        _distanceText.SetActive(false);
+        _arrow.SetActive(false);
     }
 
     public void OpenWorldmapUI(PointerEventData evt)
@@ -157,7 +213,7 @@ public class UI_Worldmap : UI_Scene
     }
     */
     
-    public bool CheckWeather()
+    public bool CheckWeather()//날씨 체크
     {
         for (int i = 1; i <= Managers.Data.WorldmapDict.Count; i++)
         {
@@ -169,14 +225,14 @@ public class UI_Worldmap : UI_Scene
         return false;
     }
 
-    public bool CheckPosition(int i)
+    public bool CheckPosition(int i)//특정 좌표에 들어왔는지 확인
     {
         if (_ship.transform.position.x >= Managers.Data.WorldmapDict[i].minX
             && _ship.transform.position.x <= Managers.Data.WorldmapDict[i].maxX
             && _ship.transform.position.y >= Managers.Data.WorldmapDict[i].minY
             && _ship.transform.position.y <= Managers.Data.WorldmapDict[i].maxY)
         {
-            _weather.GetComponent<TMP_Text>().SetText(Managers.Data.WorldmapDict[i].name + " " + Managers.Data.WorldmapDict[i].weather);
+            _weatherText.GetComponent<TMP_Text>().SetText(Managers.Data.WorldmapDict[i].name + " " + Managers.Data.WorldmapDict[i].weather);
             return true;
         }
         else
