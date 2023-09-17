8a5bb3aa1488748bcda4d6aa20f0b73e212df7a2
KSangMin <fhfpszmfnwm@naver.com>
2023-09-17 20:04:33 +0900

    fix: 폴더 구조 수정 후 버그 수정
    fix: 폴더 구조 수정 후 버그 수정

--- Assets/Scripts/Managers/Content/ArtifactManager.cs
+++ Assets/Scripts/Managers/Content/ArtifactManager.cs
@@ -38,7 +38,7 @@ public class ArtifactManager
             }
         }
         Array.Resize(ref artifactScrolls, artifactScrolls.Length + 1);
-        artifactScrolls[artifactScrolls.Length - 1] = Managers.Resource.Load<Artifact>($"Prefabs/Interactable/Artifacts/{path}");
+        artifactScrolls[artifactScrolls.Length - 1] = Managers.Resource.Load<Artifact>($"Prefabs/Objects/NonCharacter/Interactable/Artifacts/{path}");
     }
 
     public UI_ArtifactSlot MakeArtifactSlot(Transform parent = null)
